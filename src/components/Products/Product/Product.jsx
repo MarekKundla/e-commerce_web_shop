a73f54013cdeb443ad1c86dc5fe19d85f4a404e5
Marek Kundla <Marek.Kundla@gmail.com>
2023-08-21 17:40:15 +0200

    project setup

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {
+    Card,
+    CardMedia,
+    CardContent,
+    CardActions,
+    Typography,
+    IconButton,
+} from '@mui/material'
+import { AddShoppingCard } from '@mui/icons-material'
+
+const Product = () => {
+    return (
+        <div>
+            <div>Product</div>
+        </div>
+    )
+}
+
+export default Product
